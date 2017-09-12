72ee154b83f65ec7e32d7bb370ade8c7a478d473
Théo Chassaigne <theo.chassaigne@immersion.fr>
2017-09-12 13:50:52 +0200

    First commit

---
+++ Editor/EditorWindowTimer.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorWindowTimer {
+
+	public enum TimerStatus
+    {
+        NotStarted,
+        Running,
+        Over
+    }
+
+    private TimerStatus currentStatus = TimerStatus.NotStarted;
+    /// <summary>
+    /// Timer duration (in seconds)
+    /// </summary>
+    private float length = 0;
+    private float elapsedTime = 0;
+    private float stepPrecision = .01f;
+
+    public TimerStatus CurrentStatus
+    {
+        get
+        {
+            return currentStatus;
+        }
+
+        private set
+        {
+            currentStatus = value;
+        }
+    }
+
+    public float Length
+    {
+        get
+        {
+            return length;
+        }
+
+        set
+        {
+            if(CurrentStatus == TimerStatus.Running)
+            {
+                ElapsedTime = 0;
+            }
+
+            length = value;
+        }
+    }
+
+    public float ElapsedTime
+    {
+        get
+        {
+            return elapsedTime;
+        }
+
+        private set
+        {
+            elapsedTime = value;
+        }
+    }
+
+    public float StepPrecision
+    {
+        get
+        {
+            return stepPrecision;
+        }
+
+        set
+        {
+            stepPrecision = value;
+        }
+    }
+
+    public EditorWindowTimer(float length, float stepPrecision = .01f)
+    {
+        this.Length = length;
+        this.StepPrecision = stepPrecision;
+    }
+
+    public void Reset()
+    {
+        this.CurrentStatus = TimerStatus.NotStarted;
+        this.ElapsedTime = 0;
+    }
+
+    public void Start()
+    {
+        this.CurrentStatus = TimerStatus.Running;
+    }
+
+    public void Pause()
+    {
+        this.CurrentStatus = TimerStatus.NotStarted;
+    }
+
+    public TimerStatus Update()
+    {
+        if (this.CurrentStatus == TimerStatus.Running)
+        {
+            this.ElapsedTime += this.StepPrecision;
+
+            if(this.ElapsedTime >= this.Length)
+            {
+                this.CurrentStatus = TimerStatus.Over;
+            }
+        }
+
+        return this.CurrentStatus;
+
+    }
+}
---
+++ Editor/MeasureEntry.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MeasureEntry{
+	private GameObject a, b;
+	private float fDistance;
+	private Vector3 vec3Distance;
+
+	public Color Color;
+	public bool Shown = true;
+
+	public GameObject B {
+		get {
+			return b;
+		}
+		set {
+			b = value;
+			UpdateDistance ();
+		}
+	}
+
+	public GameObject A {
+		get {
+			return a;
+		}
+		set {
+			a = value;
+			UpdateDistance ();
+		}
+	}
+
+	public float FDistance {
+		get {
+			return fDistance;
+		}
+	}
+
+	public Vector3 Vec3Distance {
+		get {
+			return vec3Distance;
+		}
+	}
+
+	public MeasureEntry(){
+		a = null;
+		b = null;
+		fDistance = 0f;
+		Color = Color.white;
+	}
+
+	public void UpdateDistance(){
+		if (a != null && b != null) {
+			fDistance = Vector3.Distance (b.transform.position, a.transform.position);
+
+			vec3Distance = b.transform.position - a.transform.position;
+            vec3Distance.x = Mathf.Abs(vec3Distance.x);
+            vec3Distance.y = Mathf.Abs(vec3Distance.y);
+            vec3Distance.z = Mathf.Abs(vec3Distance.z);
+        }
+	}
+}
